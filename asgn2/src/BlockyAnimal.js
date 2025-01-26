461d7fb0d257ee4525c2ca562e9b909496b386a9
jlin216 <jlin216@ucsc.edu>
2025-01-25 20:18:46 -0800

    im cooked

--- asgn2/src/BlockyAnimal.js
+++ asgn2/src/BlockyAnimal.js
@@ -32,7 +32,9 @@ let u_GlobalRotateMatrix;
 let g_shapesList = [];
 let g_selectedColor = [.5, .5, .5, 1.0];
 let g_selectedType = POINT;
-let g_globalAngle = 0;
+let g_globalAngleX = 90;
+let g_globalAngleY = 15;
+let g_wingAngle = 0;
 
 
 function setupWebGL() {
@@ -41,7 +43,7 @@ function setupWebGL() {
 
   // Get the rendering context for WebGL
   // gl = getWebGLContext(canvas);
-  gl = canvas.getContext("webgl", { preserveDrawingBuffer: true });
+  gl = canvas.getContext("webgl", { preserveDrawingMainBuffer: true });
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
@@ -106,42 +108,98 @@ function renderHead() {
   head.color = [1, 1, 1, 1];
   head.matrix.translate(-.6, 0.05, .025);
   head.matrix.rotate(120, 0, 0, 1);
-  head.matrix.scale(0.15, .15, .15);
+  head.matrix.scale(0.1, .1, .1);
   head.render();
 
   var head2 = new Cube();
   head2.color = [1, 1, 1, 1];
   head2.matrix.translate(-.5, .1, 0);
   head2.matrix.rotate(120, 0, 0, 1);
-  head2.matrix.scale(.2, .2, .2);
+  head2.matrix.scale(.15, .15, .15);
   head2.render();
 
   var neck = new Cube();
   neck.color = [1, 1, 1, 1];
   neck.matrix.translate(-0.5, -.3, 0);
   neck.matrix.rotate(15, 0, 0, 1);
-  neck.matrix.scale(.15, .5, .2);
+  neck.matrix.scale(.1, .45, .15);
   neck.render();
 }
 
 function renderBody() {
   var belly = new Cube();
   belly.color = [1, 0, 0, 1];
-  belly.matrix.translate(-0.5, -.5, -.1);
-  belly.matrix.scale(.7, .3, .4);
+  belly.matrix.translate(-0.5, -.5, -.05);
+  belly.matrix.scale(.65, .25, .25);
   belly.render();
 }
 
+function renderLeftWing() {
+  var wingMain = new Cube();
+  wingMain.color = [1,1,0,1];
+  //wingMain.matrix.setTranslate(-0.4,-.3,-.45);
+  //wingMain.matrix.translate(-0.4,-.3,-.45);
+  wingMain.matrix.rotate(45,1,0,0)
+  wingMain.matrix.translate(-0.4,-.3,-.45);
+  var wingMainLoc = new Matrix4(wingMain.matrix);
+  wingMain.matrix.scale(.15,.05,.65);
+  wingMain.render();
+
+  var wingMid = new Cube();
+  wingMid.matrix = wingMainLoc;
+  wingMid.color = [0,1,0,1];
+  wingMid.matrix.rotate(90,1,0,0);
+  var wingMidLoc = new Matrix4(wingMid.matrix);
+  wingMid.matrix.scale(.15,.05,.45);
+  wingMid.render();
+
+  var wingTip = new Cube();
+  wingTip.matrix = wingMidLoc;
+  wingTip.color = [0,0,1,1];
+  wingTip.matrix.rotate(90,1,0,0);
+  wingTip.matrix.translate(-.0,.45,-0.05);
+  wingTip.matrix.scale(.15,.05,.15);
+  wingTip.render();
+}
+
+function renderRightWing() {
+  var wingMain = new Cube();
+  wingMain.color = [1,1,0,1];
+  wingMain.matrix.rotate(180,0,1,0).rotate(45,1,0,0);
+  wingMain.matrix.translate(.25,-.4,-.55);
+  var wingMainLoc = new Matrix4(wingMain.matrix);
+  wingMain.matrix.scale(.15,.05,.65);
+  wingMain.render();
+
+  var wingMid = new Cube();
+  wingMid.matrix = wingMainLoc;
+  wingMid.color = [0,1,0,1];
+  wingMid.matrix.rotate(90,1,0,0);
+  var wingMidLoc = new Matrix4(wingMid.matrix);
+  wingMid.matrix.scale(.15,.05,.45);
+  wingMid.render();
+
+  var wingTip = new Cube();
+  wingTip.matrix = wingMidLoc;
+  wingTip.color = [0,0,1,1];
+  wingTip.matrix.rotate(90,1,0,0);
+  wingTip.matrix.translate(-.0,.45,-0.05);
+  wingTip.matrix.scale(.15,.05,.15);
+  wingTip.render();
+}
 function renderAllShapes() {
   // Check the time at the start of this fuction
   var startTime = performance.now();
-  var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
+  var globalRotMat = new Matrix4().rotate(g_globalAngleX, 0, 1, 0).rotate(g_globalAngleY,1,0,0);
+  
   gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   renderHead();
   renderBody();
+  renderLeftWing();
+  renderRightWing();
   // var len = g_shapesList.length;
   // for (var i = 0; i < len; i++) {
   //   g_shapesList[i].render();
@@ -176,8 +234,8 @@ function renderAllShapes() {
 
 function addActionsForHtmlUI() {
   // RGB Sliders
-  document.getElementById('Cam').addEventListener('mousemove', function () { g_globalAngle = this.value; renderAllShapes(); });
-  document.getElementById('Red').addEventListener('mouseup', function () { g_selectedColor[0] = this.value / 100; });
+  document.getElementById('Cam').addEventListener('mousemove', function () { g_globalAngleX = this.value; renderAllShapes(); });
+  document.getElementById('wing').addEventListener('mousemove', function () { g_wingAngle = this.value; renderAllShapes();});
   document.getElementById('Green').addEventListener('mouseup', function () { g_selectedColor[1] = this.value / 100; });
   document.getElementById('Blue').addEventListener('mouseup', function () { g_selectedColor[2] = this.value / 100; });
   // Clear Button
@@ -191,9 +249,18 @@ function main() {
   connectVariablesToGLSL();
 
   addActionsForHtmlUI();
-  // Register function (event handler) to be called on a mouse press
-  canvas.onmousemove = function (ev) {g_globalAngle = ev.clientX };
+  canvas.addEventListener('mousemove', (ev) => {
 
+    // Normalize coordinates (-1 to 1 range)
+    const xRatio = (ev.clientX / canvas.width) * 2 - 1;
+    const yRatio = (ev.clientY / canvas.height) * 2 - 1;
+
+    g_globalAngleX = xRatio * 360;
+    g_globalAngleY = yRatio * 360; 
+
+    // Re-render shapes with updated angles
+    renderAllShapes();
+  });
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
