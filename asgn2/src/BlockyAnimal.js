a59b34c33119abfa633e9c509e4d1ae7bbac5027
jlin216 <jlin216@ucsc.edu>
2025-01-26 20:06:56 -0800

    smoother animation

--- asgn2/src/BlockyAnimal.js
+++ asgn2/src/BlockyAnimal.js
@@ -32,14 +32,15 @@ let u_GlobalRotateMatrix;
 let g_shapesList = [];
 let g_selectedColor = [.5, .5, .5, 1.0];
 let g_selectedType = POINT;
-let g_globalAngleX = 90;
+let g_globalAngleX = 200;
 let g_globalAngleY = 15;
 let g_mainWingAngle = 225;
-let g_midWingAngle = 0 - 90;
+let g_midWingAngle = -90;
 let g_tipWingAngle = 50;
 let g_startTime = performance.now() / 1000.0;
 let g_seconds = performance.now() / 1000.0 - g_startTime;
 let g_animation = false;
+let g_showBone = 0.1;
 function setupWebGL() {
   // Retrieve <canvas> element
   canvas = document.getElementById('webgl');
@@ -53,8 +54,8 @@ function setupWebGL() {
   }
   // enables blending for opacity
   gl.enable(gl.DEPTH_TEST);
-  // gl.enable(gl.BLEND);
-  // gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+  gl.enable(gl.BLEND);
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 }
 
 function connectVariablesToGLSL() {
@@ -108,38 +109,87 @@ function convertMouseCoordinatesToGL(ev) {
 
 function renderHead() {
   var head = new Cube();
-  head.color = [1, 1, 1, 1];
+  head.color = [1, .7, 1, 1];
   head.matrix.translate(-.6, 0.05, .025);
   head.matrix.rotate(120, 0, 0, 1);
   head.matrix.scale(0.1, .1, .1);
   head.render();
 
   var head2 = new Cube();
-  head2.color = [1, 1, 1, 1];
+  head2.color = [1, .8, 1, 1];
   head2.matrix.translate(-.5, .1, 0);
   head2.matrix.rotate(120, 0, 0, 1);
   head2.matrix.scale(.15, .15, .15);
   head2.render();
 
   var neck = new Cube();
-  neck.color = [1, 1, 1, 1];
+  neck.color = [1, .8, 1, 1];
   neck.matrix.translate(-0.5, -.3, 0);
   neck.matrix.rotate(15, 0, 0, 1);
   neck.matrix.scale(.1, .45, .15);
   neck.render();
+  var beak = new Cone();
+  beak.color = [1, .5, 0, 1];
+  beak.height = .15;
+  beak.segments = 10;
+  beak.radius = 15;
+  beak.matrix.translate(-.7, .05, 0.08);
+  beak.matrix.rotate(120, 0, 0, 1);
+  beak.render();
+}
+
+function createBodyFeathers(loc, r) {
+  var cone = new Cone();
+  cone.color = [1, .7, 1, 1];
+  cone.height = .2;
+  cone.segments = 3;
+  cone.radius = 20;
+  cone.matrix.translate(loc[0], loc[1], loc[2]);
+  cone.matrix.rotate(r[0], r[1], r[2], r[3]);
+  cone.render();
 }
 
 function renderBody() {
   var belly = new Cube();
-  belly.color = [1, 0, 0, 1];
+  belly.color = [1, .8, 1, 1];
   belly.matrix.translate(-0.5, -.5, -.05);
   belly.matrix.scale(.65, .25, .25);
   belly.render();
+
+  for (let i = 0; i < 6; i++) {
+    createBodyFeathers([-0.45 + i / 10, -.45, 0.2], [250, 0, 0, 1]);
+    createBodyFeathers([-0.45 + i / 10, -.35, 0.2], [250, 0, 0, 1]);
+    createBodyFeathers([-0.45 + i / 10, -.25, 0.2], [250, 0, 0, 1]);
+    createBodyFeathers([-0.45 + i / 10, -.25, 0.07], [250, 0, 0, 1]);
+    createBodyFeathers([-0.45 + i / 10, -.25, -0.05], [250, 0, 0, 1]);
+    createBodyFeathers([-0.45 + i / 10, -.35, -0.05], [250, 0, 0, 1]);
+    createBodyFeathers([-0.45 + i / 10, -.45, -0.05], [250, 0, 0, 1]);
+  }
+  for (let i = 0; i < 3; i++) {
+    createBodyFeathers([-0.45, -.25 - i / 13, 0.07], [150, 0, 0, 1]);
+    createBodyFeathers([-0.45, -.25 - i / 13, 0.15], [150, 0, 0, 1]);
+    createBodyFeathers([-0.45, -.25 - i / 13, -0.0], [150, 0, 0, 1]);
+  }
+  for (let i = 0; i < 3; i++) {
+    createBodyFeathers([.15, -.3 - i / 13, 0.07], [230, 0, 0, 1]);
+    createBodyFeathers([.15, -.3 - i / 13, 0.15], [230, 0, 0, 1]);
+    createBodyFeathers([.15, -.3 - i / 13, -0.0], [230, 0, 0, 1]);
+  }
+}
+
+function createFeather(bone, loc, s, r) {
+  var feather = new Cube();
+  feather.color = [0, 1, 1, 1];
+  feather.matrix = new Matrix4(bone);
+  feather.matrix.translate(loc[0], loc[1], loc[2]);
+  feather.matrix.rotate(r[0], r[1], r[2], r[3]);
+  feather.matrix.scale(s[0], s[1], s[2]);
+  feather.render();
 }
 
 function renderLeftWing() {
   var wingMain = new Cube();
-  wingMain.color = [1, 1, 0, 1];
+  wingMain.color = [1, 1, 0, g_showBone];
   //wingMain.matrix.setTranslate(-0.4,-.3,-.45);
   wingMain.matrix.translate(-0.4, -.3, 0);
   wingMain.matrix.rotate(g_mainWingAngle, 1, 0, 0)
@@ -148,9 +198,12 @@ function renderLeftWing() {
   wingMain.matrix.scale(.15, .05, .45);
   wingMain.render();
 
+  //createFeather(wingMainLoc, [0,-.55,0], [.2,.05,.05], [0,1,0,0]);
+  // createFeather(wingMainLoc, [0,-.05,0.05], [.2,.05,.05]);
+  // createFeather(wingMainLoc, [0,-.05,0.1], [.2,.05,.05]);
   var wingMid = new Cube();
-  wingMid.matrix = wingMainLoc;
-  wingMid.color = [0, 1, 0, 1];
+  wingMid.matrix = new Matrix4(wingMainLoc);
+  wingMid.color = [0, 1, 0, g_showBone];
   wingMid.matrix.translate(0, 0, .45);
   wingMid.matrix.rotate(g_midWingAngle, 1, 0, 0);
   var wingMidLoc = new Matrix4(wingMid.matrix);
@@ -158,18 +211,31 @@ function renderLeftWing() {
   wingMid.render();
 
   var wingTip = new Cube();
-  wingTip.matrix = wingMidLoc;
-  wingTip.color = [0, 0, 1, 1];
+  wingTip.matrix = new Matrix4(wingMidLoc);
+  wingTip.color = [0, 0, 1, g_showBone];
   wingTip.matrix.translate(0, 0, .4);
   wingTip.matrix.rotate(g_tipWingAngle, 1, 0, 0);
+  var wingTipLoc = new Matrix4(wingTip.matrix);
   wingTip.matrix.scale(.15, .05, .2);
   wingTip.render();
+
+  // feathers along the main wing
+  for (let i = 0; i < 10; i++) {
+    createFeather(wingMainLoc, [0.05, -.05, 0 + i / 20], [.2 + i / 50, .05, .05], [0, 1, 0, 0]);
+  }
+  for (let i = 0; i < 7; i++) {
+    createFeather(wingMidLoc, [0.05, -.05, 0 + i / 20], [.35 + i / 50, .05, .05], [0, 1, 0, 0]);
+  }
+  for (let i = 0; i < 5; i++) {
+    createFeather(wingTipLoc, [0.05, -.05, -0.1 + i / 20], [.45 + i / 30, .05, .05], [-10 - i * 10, 0, 1, 0]);
+    // createFeather(wingTipLoc, [0.05, -.05, 0], [.35, .05, .05], [-20, 0, 1, 0]);
+  }
 }
 
 function renderRightWing() {
   //wingMain.matrix.rotate(180,0,1,0).rotate(45,1,0,0);
   var wingMain = new Cube();
-  wingMain.color = [1, 1, 0, 1];
+  wingMain.color = [1, 1, 0, g_showBone];
   //wingMain.matrix.setTranslate(-0.4,-.3,-.45);
   wingMain.matrix.translate(-0.25, -.3, 0.15);
   wingMain.matrix.rotate(180, 0, 1, 0).rotate(g_mainWingAngle, 1, 0, 0)
@@ -177,10 +243,13 @@ function renderRightWing() {
   var wingMainLoc = new Matrix4(wingMain.matrix);
   wingMain.matrix.scale(.15, .05, .45);
   wingMain.render();
-
+  // feathers along the main wing
+  // for(let i = 0; i < 40; i++) {
+  //   createFeather(wingMainLoc, [0.1,-.05,0 + i/100], [.2+ i/300,.05,.05], [180,0,1,0]);
+  // }
   var wingMid = new Cube();
-  wingMid.matrix = wingMainLoc;
-  wingMid.color = [0, 1, 0, 1];
+  wingMid.matrix = new Matrix4(wingMainLoc);
+  wingMid.color = [0, 1, 0, g_showBone];
   wingMid.matrix.translate(0, 0, .45);
   wingMid.matrix.rotate(g_midWingAngle, 1, 0, 0);
   var wingMidLoc = new Matrix4(wingMid.matrix);
@@ -188,36 +257,29 @@ function renderRightWing() {
   wingMid.render();
 
   var wingTip = new Cube();
-  wingTip.matrix = wingMidLoc;
-  wingTip.color = [0, 0, 1, 1];
+  wingTip.matrix = new Matrix4(wingMidLoc);
+  wingTip.color = [0, 0, 1, g_showBone];
   wingTip.matrix.translate(0, 0, .4);
   wingTip.matrix.rotate(g_tipWingAngle, 1, 0, 0);
+  var wingTipLoc = new Matrix4(wingTip.matrix);
   wingTip.matrix.scale(.15, .05, .2);
   wingTip.render();
-  // var wingMain = new Cube();
-  // wingMain.color = [1,1,0,1];
-  // wingMain.matrix.rotate(180,0,1,0).rotate(45,1,0,0);
-  // wingMain.matrix.translate(.25,-.4,-.55);
-  // var wingMainLoc = new Matrix4(wingMain.matrix);
-  // wingMain.matrix.scale(.15,.05,.65);
-  // wingMain.render();
 
-  // var wingMid = new Cube();
-  // wingMid.matrix = wingMainLoc;
-  // wingMid.color = [0,1,0,1];
-  // wingMid.matrix.rotate(90,1,0,0);
-  // var wingMidLoc = new Matrix4(wingMid.matrix);
-  // wingMid.matrix.scale(.15,.05,.45);
-  // wingMid.render();
-
-  // var wingTip = new Cube();
-  // wingTip.matrix = wingMidLoc;
-  // wingTip.color = [0,0,1,1];
-  // wingTip.matrix.rotate(90,1,0,0);
-  // wingTip.matrix.translate(-.0,.45,-0.05);
-  // wingTip.matrix.scale(.15,.05,.15);
-  // wingTip.render();
+  for (let i = 0; i < 10; i++) {
+    createFeather(wingMainLoc, [0.1, -.05, .05 + i / 20], [.2 + i / 50, .05, .05], [180, 0, 1, 0]);
+  }
+  // for (let i = 0; i < 10; i++) {
+  //   createFeather(wingMainLoc, [0.05, -.05, 0 + i / 20], [.2 + i / 50, .05, .05], [0, 1, 0, 0]);
+  // }
+  for (let i = 0; i < 7; i++) {
+    createFeather(wingMidLoc, [0.1, -.05, 0.05 + i / 20], [.35 + i / 50, .05, .05], [180, 0, 1, 0]);
+  }
+  for (let i = 0; i < 5; i++) {
+    createFeather(wingTipLoc, [0.1, -.05, -0.05 + i / 20], [.45 + i / 30, .05, .05], [180 + 10 + i * 10, 0, 1, 0]);
+    // createFeather(wingTipLoc, [0.05, -.05, 0], [.35, .05, .05], [-20, 0, 1, 0]);
+  }
 }
+
 function renderAllShapes() {
   // Check the time at the start of this fuction
   var startTime = performance.now();
@@ -231,6 +293,13 @@ function renderAllShapes() {
   renderBody();
   renderLeftWing();
   renderRightWing();
+
+  // var cone = new Cone();
+  // cone.color = [1,1,0,1];
+  // cone.segments = 3;
+  // cone.height = .3;
+  // cone.radius = 20;
+  // cone.render();
   // var len = g_shapesList.length;
   // for (var i = 0; i < len; i++) {
   //   g_shapesList[i].render();
